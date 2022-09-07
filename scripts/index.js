cea29df2291667d3b4ec967f041eae0654b69bc8
jendoc <jaharrisnv@gmail.com>
2022-09-07 00:00:38 -0700

    feat: responsive results markup

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,5 @@
 const navButton = document.querySelector(".nav__menu-button");
 const mobileNavMenu = document.querySelector(".modal");
-let modalLink = document.querySelectorAll(".modal__link");
 
 function toggleNav() {
   mobileNavMenu.classList.toggle("modal__opened");
